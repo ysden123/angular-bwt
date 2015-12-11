5c3d26d56ac249752f68a979b72c4c6436c51feb
ysden123 <ysden123@gmail.com>
2015-12-11 17:20:36 +0200

    clean

--- WebContent/dropdownSiteMenu/menuController.js
+++ WebContent/dropdownSiteMenu/menuController.js
@@ -26,35 +26,4 @@ app.controller("DropdownSiteMenuController", function($scope, $http) {
 			action : "cs"
 		}]
 	}];
-	/****************************************************
-	,
-	{
-	    title : "Menu1",
-	    action : "#",
-	    menus : [{
-	        title : "Submenu 1a",
-	        action : "stuff"
-	    }, {
-	        title : "Submenu 1b",
-	        action : "moreStuff",
-	        menus : [{
-	            title : "Submenu 1b 1",
-	            action : "stuff"
-	        }, {
-	            title : "Submenu 1b 2",
-	            action : "moreStuff"
-	        }]
-	    }]
-	}, {
-	    title : "Menu2",
-	    action : "#",
-	    menus : [{
-	        title : "Submenu 2a",
-	        action : "awesomeStuff"
-	    }, {
-	        title : "Submenu 2b",
-	        action : "moreAwesomeStuff"
-	    }]
-	}]
-	*****************************************/
 });
